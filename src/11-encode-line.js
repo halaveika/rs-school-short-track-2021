667143c32b2877d052a78375ca76ef30de14b910
halaveika <halaveika1988@gmail.com>
2021-04-12 01:00:09 +0300

    feat: add 11-encode-line

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,17 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const temp = str.split('');
+  const result = [];
+  let counter = 1;
+  for (let i = 1; i <= temp.length; i++) {
+    if (temp[i] === temp[i - 1]) counter++;
+    else if (counter > 1) {
+      result.push(counter, temp[i - 1]);
+      counter = 1;
+    } else result.push(temp[i - 1]);
+  }
+  return result.join('');
 }
-
 module.exports = encodeLine;
